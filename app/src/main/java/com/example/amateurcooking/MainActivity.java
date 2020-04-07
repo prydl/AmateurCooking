8024c37a85ead999a2cd3cd3735448e3b7e49d87
PRIYAL DALAL <priyaljdalal@gmail.com>
2020-04-07 20:39:10 +1000

    Login Page modified but not finished

--- app/src/main/java/com/example/amateurcooking/MainActivity.java
+++ app/src/main/java/com/example/amateurcooking/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.amateurcooking;
 import androidx.appcompat.app.AppCompatActivity;
 
 //add import statements for widgets and views
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
@@ -10,87 +12,65 @@ import android.view.View.OnClickListener;
 import android.os.Bundle;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-    }
 
     //initialise fields
     private EditText emailfield;
     private EditText passwordfield;
     private Button signinbutton;
-    private TextView feedbackText;
+    private TextView test;
+    private int counter = 3;
 
-    //find views by id
-    private void findViews() {
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         emailfield = findViewById(R.id.emailfield);
-        //setting sample email
-        emailfield.setText("johnsmith@gmail.com");
-
         passwordfield = findViewById(R.id.passwordfield);
-        //setting sample password
-        passwordfield.setText("password123");
-
-        feedbackText = findViewById(R.id.test);
-        feedbackText.setText("Waiting...");
-        // this should work easily though i don't understand why
-        // going to try and understand this better
-
+        test = findViewById(R.id.test);
         signinbutton = findViewById(R.id.signinbutton);
-        signinbutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-                feedbackText.setText("You clicked the button");
-                // tbh, i'm not sure why this isn't working here
-                // its just asking for the button to be pressed
-                //its being a bitch
-                // i agree
 
+        test.setText("Number of attempts remaining: 3");
+
+
+        signinbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Validate(emailfield.getText().toString(), passwordfield.getText().toString());
+                //feedbackText.setText("Incorrect Password");
             }
         });
+
     }
 
-    public MainActivity(TextView feedbackText) {
-        this.feedbackText = feedbackText;
-    }
+    // writing a method to validate username and password
+    @SuppressLint("SetTextI18n")
+    private void Validate(String userEmail, String userPassword) {
+        if ((userEmail.equals("johnsmith@gmail.com") && (userPassword.equals("password123")))){
+            Intent intent = new Intent(MainActivity.this, Dashboard.class);
+            startActivity(intent);
+        }else{
+            counter--;
 
-    public TextView getTest() {
-        return feedbackText;
-    }
+            test.setText("Number of attempts remaining: " + counter);
 
-    public void setTest(TextView feedbackText) {
-        this.feedbackText = feedbackText;
-    }
-
-    /*
-    //@Override
-    //handle the action upon clicking the sign in button
-    public void onClick(View v) {
-
-        if (v == signinbutton) {
-            // Handle clicks for sign in button
-            if (emailfield.getText().toString().equals("johnsmith@gmail.com") && passwordfield.getText().toString().equals("password123")) {
-                //do what
-                feedbackText.setText("Success");
-            } else {
-                feedbackText.setText("Fail");
+            if(counter == 0){
+                 signinbutton.setEnabled(false);
             }
 
-        } else {
-            feedbackText.getText();
-
         }
-
-        //assume login works and moves onto the next page
-        //for now, connect the sign in button with the dashboard page
-
-    }*/
-
     }
+
+
+}
+
+
+
+
+
 
 
 
