dabcb0df96bf390211a990c3684e30a14581f2b2
PRIYAL DALAL <priyaljdalal@gmail.com>
2020-04-17 15:30:25 +1000

    Fixing Quiz UI.

--- app/src/main/java/com/example/amateurcooking/level1.java
+++ app/src/main/java/com/example/amateurcooking/level1.java
@@ -1,14 +1,37 @@
 package com.example.amateurcooking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class level1 extends AppCompatActivity {
+public class level1 extends AppCompatActivity implements OnClickListener {
+    //declare the cards
+    private Button quizButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1);
+
+        //setting views references for each card view object
+        quizButton = findViewById(R.id.quizButton);
+
+        //adding onClickListeners
+        quizButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, quiz1.class);
+        this.startActivity ( intent );
+
+
+
     }
 }
