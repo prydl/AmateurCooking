b0b81c9109f560ab1b03d6cc367eb1c2ebb368e2
PRIYAL DALAL <priyaljdalal@gmail.com>
2020-04-24 14:14:22 +1000

    Achievements Page.

---
+++ app/src/main/java/com/example/amateurcooking/ProgressPage.java
@@ -0,0 +1,14 @@
+package com.example.amateurcooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProgressPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress_page);
+    }
+}
