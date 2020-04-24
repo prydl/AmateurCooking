f65349b3dd00303e74731ef0018b5f762169ab5a
PRIYAL DALAL <priyaljdalal@gmail.com>
2020-04-24 18:45:13 +1000

    Achievements data populated.

--- app/src/main/java/com/example/amateurcooking/Achievements.java
+++ app/src/main/java/com/example/amateurcooking/Achievements.java
@@ -24,24 +24,24 @@ public class Achievements extends AppCompatActivity {
         AchievementsRecyclerView = findViewById(R.id.achievementsrv);
         mData = new ArrayList<>();
 
-        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "24/04/2020", R.drawable.achievement));
+        mData.add(new AchievementsItem("Cake Face", "Vanilla Cake? Now you know the basics of every cake!", "24/04/2020", R.drawable.achievement));
         mData.add(new AchievementsItem("HIGH TEMP!", "Achievement unlocked for making 3 baking recipes!", "22/04/2020", R.drawable.achievement));
         mData.add(new AchievementsItem("@markhill says:", "John, did you try the garlic potatoes recipe?", "21/04/2020", R.drawable.profile1));
-        mData.add(new AchievementsItem("Your recipe was favourited by @markhill", "Amazing work!", "20/04/2020", R.drawable.star));
-        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "21/04/2020", R.drawable.achievement));
-        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "21/04/2020", R.drawable.achievement));
-        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "21/04/2020", R.drawable.achievement));
-        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "21/04/2020", R.drawable.achievement));
-        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "21/04/2020", R.drawable.achievement));
-        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "21/04/2020", R.drawable.achievement));
-        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "21/04/2020", R.drawable.achievement));
-        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "21/04/2020", R.drawable.achievement));
-        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "21/04/2020", R.drawable.achievement));
-        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "21/04/2020", R.drawable.achievement));
-        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "21/04/2020", R.drawable.achievement));
-        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "21/04/2020", R.drawable.achievement));
-        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "21/04/2020", R.drawable.achievement));
-        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "21/04/2020", R.drawable.achievement));
+        mData.add(new AchievementsItem("Your recipe was upvoted by @markhill", "Amazing work!", "20/04/2020", R.drawable.star));
+        mData.add(new AchievementsItem("Valiant Visitor", "Good work on visiting your friends progresses!", "19/04/2020", R.drawable.achievement));
+        mData.add(new AchievementsItem("Pushing Through!", "We know you worked extra hard on that recipe!", "17/04/2020", R.drawable.achievement));
+        mData.add(new AchievementsItem("@laura35 says:", "My pesto ended up bitter...any solutions?", "16/04/2020", R.drawable.profile2));
+        mData.add(new AchievementsItem("@haileybailey says:", "John how did you make the base of your Red Chicken Curry?", "15/04/2020", R.drawable.profile2));
+        mData.add(new AchievementsItem("Winner winner, chicken dinner!", "You've become a chicken-cooking expert", "14/04/2020", R.drawable.achievement));
+        mData.add(new AchievementsItem("Level 3...Now you're free!", "Level 3 Unlocked!", "12/04/2020", R.drawable.achievement));
+        mData.add(new AchievementsItem("Tenner!", "Congratulations! You've opened 10 Recipes.", "10/04/2020", R.drawable.achievement));
+        mData.add(new AchievementsItem("@markhill says:", "I added tofu instead of fish in the tacos..you?", "05/04/2020", R.drawable.achievement));
+        mData.add(new AchievementsItem("@haileybailey says:", "I love the recipes in Level 2, and your variations!", "03/04/2020", R.drawable.profile3));
+        mData.add(new AchievementsItem("I SEA FOOD, I EAT FOOD", "First seafood recipe successfully completed!", "01/04/2020", R.drawable.achievement));
+        mData.add(new AchievementsItem("Variety!", "People loved your variation of Overnight Oats!", "30/03/2020", R.drawable.star));
+        mData.add(new AchievementsItem("LEVEL 2 HERE WE GO!", "Looking at you on your way to becoming a chef!", "24/03/2020", R.drawable.achievement));
+        mData.add(new AchievementsItem("Flaming hot!", "Using the flame for your recipes? That's hot.", "22/03/2020", R.drawable.achievement));
+        mData.add(new AchievementsItem("Welcome!", "You've opened your first recipe!", "19/03/2020", R.drawable.achievement));
 
 
         //initialise adapter
