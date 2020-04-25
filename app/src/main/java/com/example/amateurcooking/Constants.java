4105fd0a59e00f778e8280f8da4db8d84538de45
PRIYAL DALAL <priyaljdalal@gmail.com>
2020-04-25 14:20:10 +1000

    https://www.pluralsight.com/guides/making-a-notes-app-using-room-database used to make Notes application. Crashes upon launch.

---
+++ app/src/main/java/com/example/amateurcooking/Constants.java
@@ -0,0 +1,9 @@
+package com.example.amateurcooking;
+
+final public class Constants {
+    private Constants() {
+    }
+
+    public static final String TABLE_NAME_NOTE = "notes";
+    public static final String DB_NAME = "Notes";
+}
